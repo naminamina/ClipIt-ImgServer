140b2f248a9fe1e2db77e3e9340b492a0c85793a
naminamina <namikawa.mail.kyoto.mail@gmail.com>
2024-11-25 00:20:02 +0900

    アップロード機能の追加

---
+++ app/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, File, UploadFile
+import time
+import os
+import shutil 
+
+app = FastAPI()
+IMG_DIR = "./upload_img"
+os.makedirs(IMG_DIR, exist_ok=True) 
+
+def generate_id():
+    timestamp = int(time.time() * 1000)
+    return str(timestamp)
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, Clipit-ImgServer!"}
+
+
+@app.post("/upload")
+async def save_img(file: UploadFile = File(...)):
+
+    file_name = generate_id() + ".jpeg"
+    file_path = os.path.join(IMG_DIR, file_name)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"success": True}
+
+
+
+
