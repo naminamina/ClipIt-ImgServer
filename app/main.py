d977f0bff1796d40e85ac561239bd55d9dc58aa4
naminamina <namikawa.mail.kyoto.mail@gmail.com>
2024-11-27 04:10:32 +0900

    CLIPの追加

--- app/main.py
+++ app/main.py
@@ -1,65 +1,70 @@
-from fastapi import FastAPI, File, UploadFile
-import time
 import os
+import time
 import shutil 
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI()
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+import torch.nn.functional as F
+from transformers import CLIPProcessor, CLIPModel
+from PIL import Image
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
+MODEL = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+PROCESSOR = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 IMG_DIR = "./upload_img"
 os.makedirs(IMG_DIR, exist_ok=True) 
+
+app = FastAPI()
+app.mount("/upload_img", StaticFiles(directory=IMG_DIR), name="upload_img")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 def generate_id():
     timestamp = int(time.time() * 1000)
     return str(timestamp)
 
+
 class uploadResponse(BaseModel):
     similarity: float
-    rank: int
-
+    img_id: int
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Clip-imgServer"}
 
 
 @app.post("/upload", response_model=uploadResponse)
-syance def response_similarity(file: UploadFile = File(...)):
-
-    file_name = generate_id() + ".jpeg"
-    file_path = os.path.join(IMG_DIR, file_name)
-    print(file_name)
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
+def response_similarity(file: UploadFile = File(...), theme: str = Form(...)):
     try:
-        image = Image.open(file_path)
+        print(f"file:{file.filename}, theme: {theme}")  
+        img_id = generate_id()
+        file_name = img_id + ".jpeg"
+        file_path = os.path.join(IMG_DIR, file_name)
+
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        try:
+            image = Image.open(file_path)
+        except Exception as e:
+            raise HTTPException(status_code=400, detail="Invalid image file")
+
+
+
+        inputs = PROCESSOR(text=[theme], images=image, return_tensors="pt", padding=True)
+
+        outputs = MODEL(**inputs)
+        image_embeds = outputs.image_embeds
+        text_embeds = outputs.text_embeds
+
+        cosine_similarity = F.cosine_similarity(image_embeds, text_embeds)
+        similarity_percentage = cosine_similarity.item() * 100
+        print(f"cosine_similarity:{similarity_percentage}")  
+        return uploadResponse(similarity=similarity_percentage,img_id = img_id)
     except Exception as e:
-        raise HTTPException(status_code=400, detail="Invalid image file")
-
-
-
-    inputs = processor(text=["a photo of a cat"], images=image, return_tensors="pt", padding=True)
-
-    outputs = model(**inputs)
-    image_embeds = outputs.image_embeds
-    text_embeds = outputs.text_embeds
-
-    cosine_similarity = F.cosine_similarity(image_embeds, text_embeds)
-    similarity_percentage = cosine_similarity.item() * 100
-
-    rank = int(similarity_percentage // 10)
-    return uploadResponse(similarity=similarity_percentage, rank=rank)
-
-
-
-
-
+        raise HTTPException("error " + e)
--- start_local.sh
+++ start_local.sh
@@ -1,2 +1,2 @@
 # uvicorn app.main:app --reload
-uvicorn app.main:app --host 0.0.0.0 --port 80
+uvicorn app.main:app --reload --host 0.0.0.0 --port 7000
