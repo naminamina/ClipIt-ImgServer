884d3e75aef85bd7cf28b501d5e994ae756d2351
naminamina <namikawa.mail.kyoto.mail@gmail.com>
2024-11-30 04:36:28 +0900

    clipの変更

--- app/main.py
+++ app/main.py
@@ -27,44 +27,44 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# def clip_analysis(theme, img_url):
-#     try:
-#         img_request_data = requests.get(img_url)
-#         img_open_data = Image.open(BytesIO(img_request_data.content))
-#         inputs = PROCESSOR(text=[theme], images=[img_open_data], return_tensors="pt", padding=True)
-
-#         outputs = MODEL(**inputs)
-#         image_embeds = outputs.image_embeds
-#         text_embeds = outputs.text_embeds
-
-#         cosine_similarity = F.cosine_similarity(image_embeds, text_embeds)
-#         percentage_similarity = round(cosine_similarity.item() * 100, 2)
-#         return cosine_similarity
-#     except Exception  as e:
-#         logging.info(F"Clip error:{e}")
-
-
 def clip_analysis(theme, img_url):
     try:
-        img_data = requests.get(img_url)
-        img_data = Image.open(BytesIO(img_data.content))
-        # inputs = PROCESSOR(text=[theme], images=[img_data], return_tensors="pt", padding=True)
+        img_request_data = requests.get(img_url)
+        img_open_data = Image.open(BytesIO(img_request_data.content))
+        inputs = PROCESSOR(text=[theme], images=[img_open_data], return_tensors="pt", padding=True)
 
-        device = "cpu"
-        tokenizer = AutoTokenizer.from_pretrained(HF_MODEL_PATH, trust_remote_code=True)
-        processor = AutoImageProcessor.from_pretrained(HF_MODEL_PATH, trust_remote_code=True)
-        model = AutoModel.from_pretrained(HF_MODEL_PATH, trust_remote_code=True).to(device)
+        outputs = MODEL(**inputs)
+        image_embeds = outputs.image_embeds
+        text_embeds = outputs.text_embeds
 
-        image = processor(img_data, return_tensors="pt").to(device)
-        text = tokenizer([theme]).to(device)
-        image_features = model.get_image_features(**image)
-        text_features = model.get_text_features(**text)
-        cosine_similarity = F.cosine_similarity(image_features, text_features)
+        cosine_similarity = F.cosine_similarity(image_embeds, text_embeds)
         percentage_similarity = round(cosine_similarity.item() * 100, 2)
-
-        return percentage_similarity
+        return cosine_similarity
     except Exception  as e:
         logging.info(F"Clip error:{e}")
+
+
+# def clip_analysis(theme, img_url):
+#     try:
+#         img_data = requests.get(img_url)
+#         img_data = Image.open(BytesIO(img_data.content))
+#         # inputs = PROCESSOR(text=[theme], images=[img_data], return_tensors="pt", padding=True)
+
+#         device = "cpu"
+#         tokenizer = AutoTokenizer.from_pretrained(HF_MODEL_PATH, trust_remote_code=True)
+#         processor = AutoImageProcessor.from_pretrained(HF_MODEL_PATH, trust_remote_code=True)
+#         model = AutoModel.from_pretrained(HF_MODEL_PATH, trust_remote_code=True).to(device)
+
+#         image = processor(img_data, return_tensors="pt").to(device)
+#         text = tokenizer([theme]).to(device)
+#         image_features = model.get_image_features(**image)
+#         text_features = model.get_text_features(**text)
+#         cosine_similarity = F.cosine_similarity(image_features, text_features)
+#         percentage_similarity = round(cosine_similarity.item() * 100, 2)
+
+#         return percentage_similarity
+#     except Exception  as e:
+#         logging.info(F"Clip error:{e}")
         
 class uploadResponse(BaseModel):
     similarity: float
