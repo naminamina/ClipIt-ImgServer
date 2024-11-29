1456d3b1d1ef18ec8165a5667d24e0319e7b0abd
naminamina <namikawa.mail.kyoto.mail@gmail.com>
2024-11-30 03:10:53 +0900

    デバックをしてモデルを変えた

--- app/main.py
+++ app/main.py
@@ -1,24 +1,22 @@
-import os
-import time
-import shutil 
 import logging  
-from fastapi import FastAPI, File, UploadFile, Form, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
-import torch.nn.functional as F
-from transformers import CLIPProcessor, CLIPModel
-from PIL import Image
 import requests
 from io import BytesIO
+from fastapi import FastAPI, Form, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import torch
+import torch.nn.functional as F
+from transformers import AutoImageProcessor, AutoModel, AutoTokenizer
+from PIL import Image
 
-MODEL = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-PROCESSOR = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-IMG_DIR = "./upload_img"
-os.makedirs(IMG_DIR, exist_ok=True) 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# MODEL = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+# PROCESSOR = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+HF_MODEL_PATH = 'line-corporation/clip-japanese-base'
+
 
 app = FastAPI()
-app.mount("/upload_img", StaticFiles(directory=IMG_DIR), name="upload_img")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  
@@ -27,11 +25,45 @@ app.add_middleware(
     allow_headers=["*"],  
 )
 
-def generate_id():
-    timestamp = int(time.time() * 1000)
-    return str(timestamp)
+# def clip_analysis(theme, img_url):
+#     try:
+#         img_request_data = requests.get(img_url)
+#         img_open_data = Image.open(BytesIO(img_request_data.content))
+#         inputs = PROCESSOR(text=[theme], images=[img_open_data], return_tensors="pt", padding=True)
+
+#         outputs = MODEL(**inputs)
+#         image_embeds = outputs.image_embeds
+#         text_embeds = outputs.text_embeds
+
+#         cosine_similarity = F.cosine_similarity(image_embeds, text_embeds)
+#         percentage_similarity = round(cosine_similarity.item() * 100, 2)
+#         return cosine_similarity
+#     except Exception  as e:
+#         logging.info(F"Clip error:{e}")
 
 
+def clip_analysis(theme, img_url):
+    try:
+        img_data = requests.get(img_url)
+        img_data = Image.open(BytesIO(img_data.content))
+        # inputs = PROCESSOR(text=[theme], images=[img_data], return_tensors="pt", padding=True)
+
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        tokenizer = AutoTokenizer.from_pretrained(HF_MODEL_PATH, trust_remote_code=True)
+        processor = AutoImageProcessor.from_pretrained(HF_MODEL_PATH, trust_remote_code=True)
+        model = AutoModel.from_pretrained(HF_MODEL_PATH, trust_remote_code=True).to(device)
+
+        image = processor(img_data, return_tensors="pt").to(device)
+        text = tokenizer([theme]).to(device)
+        image_features = model.get_image_features(**image)
+        text_features = model.get_text_features(**text)
+        cosine_similarity = F.cosine_similarity(image_features, text_features)
+        percentage_similarity = round(cosine_similarity.item() * 100, 2)
+
+        return percentage_similarity
+    except Exception  as e:
+        logging.info(F"Clip error:{e}")
+        
 class uploadResponse(BaseModel):
     similarity: float
 
@@ -43,19 +75,14 @@ def root():
 @app.post("/upload", response_model=uploadResponse)
 
 def response_similarity(theme: str = Form(...), img_url: str = Form(...)):
+    try:
 
-    logging.info(f"images:{img_url}, theme: {theme}")  
-    img_data = requests.get(img_url)
-    img_data = Image.open(BytesIO(img_data.content))
-    inputs = PROCESSOR(text=[theme], images=[img_data], return_tensors="pt", padding=True)
-
-    outputs = MODEL(**inputs)
-    image_embeds = outputs.image_embeds
-    text_embeds = outputs.text_embeds
-
-    cosine_similarity = F.cosine_similarity(image_embeds, text_embeds)
-    similarity_percentage = cosine_similarity.item() * 100
-    logging.info(f"cosine_similarity:{similarity_percentage}")  
-    return uploadResponse(similarity=similarity_percentage)
+        logging.info(f"theme: {theme}, images:{img_url}")  
+        return_similarity = clip_analysis(theme, img_url)
+        logging.info(f"cosine_similarity:{return_similarity}")  
+        return uploadResponse(similarity=return_similarity)
 
 
+    except Exception  as e:
+        logging.info(F"Server error:{e}")
+        raise e
